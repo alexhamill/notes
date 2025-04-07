fc8455e9be67653c3788c0c38a7ca836c7a3fa75
alexhamill <alexander.hamill@icloud.com>
2025-04-07 09:20:57 -0400

    test

--- info/src/App.tsx
+++ info/src/App.tsx
@@ -1,31 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Intopage from './intropage/intropage.tsx';
-import {SignUp, SignIn} from './singuporin/signup-in.tsx';
+import Intropage from './intropage/intropage.tsx';
+import { SignUp, SignIn } from './singuporin/signup-in.tsx';
 import { UserProvider } from './base/UserContext.tsx';
 import TODO from './TODO/todo.tsx';
 import Dashboard from './dashboard/dashboard.tsx';
 
 function App() {
   return (
-    <div>
-      <UserProvider>
-        <Router>
-          <Routes>
-            <Route path='*/' element={<Intopage />}></Route>
-            <Route path='*/notes/signup' element={<div><SignUp /><SignIn /></div>}></Route>
-            <Route path='*/notes/todo' element={<TODO />}></Route>
-            <Route path='*/notes/dashboard' element={<Dashboard />}></Route>
-            <Route path='/' element={<Intopage />}></Route>
-            <Route path='/notes' element={<Intopage />}></Route>
-          </Routes>
-        </Router>
-      </UserProvider>
-    </div>
-  )
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Intropage />} />
+          <Route path="/notes/signup" element={<div><SignUp /><SignIn /></div>} />
+          <Route path="/notes/todo" element={<TODO />} />
+          <Route path="/notes/dashboard" element={<Dashboard />} />
+          {/* Redirect any unknown route back to the intro page */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </UserProvider>
+  );
 }
 
-export default App
+export default App;
