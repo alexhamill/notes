b5f731520a35f42379c44659c890bfcea3d4c92b
alexhamill <alexander.hamill@icloud.com>
2025-04-07 12:07:27 -0400

    commit myself

--- info/src/App.tsx
+++ info/src/App.tsx
@@ -1,21 +1,21 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Intropage from './intropage/intropage.tsx';
+import IntroPage from './intropage/intropage.tsx';
 import { SignUp, SignIn } from './singuporin/signup-in.tsx';
 import { UserProvider } from './base/UserContext.tsx';
 import TODO from './TODO/todo.tsx';
 import Dashboard from './dashboard/dashboard.tsx';
+import './App.css';
 
 function App() {
   return (
     <UserProvider>
       <Router>
         <Routes>
-          <Route path="/notes" element={<Intropage />} />
-          <Route path="/notes/signup" element={<div><SignUp /><SignIn /></div>} />
-          <Route path="/notes/todo" element={<TODO />} />
-          <Route path="/notes/dashboard" element={<Dashboard />} />
-          {/* Redirect any unknown route back to the intro page */}
-          <Route path="*" element={<Navigate to="/notes" replace />} />
+          <Route path="/" element={<IntroPage />} />
+          <Route path="/notes/signup/*" element={<div><SignUp /><SignIn /></div>} />
+          <Route path="/notes/todo/*" element={<TODO />} />
+          <Route path="/notes/dashboard/*" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
     </UserProvider>
--- info/src/intropage/intropage.tsx
+++ info/src/intropage/intropage.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./intropage.css";
-import { Route } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Intropage: React.FC = () => {
+    const navigate = useNavigate();
+    
+
     return (
         <div className="intro-page">
             <h1>Hello Welcome to my Note Taking Page</h1>
             <p>I build to for my own personal use but of course i will make it so everyone can use it if i can.</p>
             <p>so far there is nothing</p>
-            <button onClick={() => window.location.href = 'notes/signup'}>Get Started</button>
+            <button onClick={() => navigate('/notes/signup')}>Get Started</button>
+            <Link to="/notes/signup">Go to Dashboard</Link>
         </div>
     );
   };
