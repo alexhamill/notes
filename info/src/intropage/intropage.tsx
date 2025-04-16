271d9bdcb85ed9352e19c5e636405f7ba72103ba
alexhamill <alexander.hamill@icloud.com>
2025-04-16 00:50:50 -0400

    remoove message

--- info/src/intropage/intropage.tsx
+++ info/src/intropage/intropage.tsx
@@ -9,9 +9,7 @@ const Intropage: React.FC = () => {
 
     return (
         <div className="intro-page">
-            <h1>Hello Welcome to my Note Taking Page</h1>
-            <p>I build to for my own personal use but of course i will make it so everyone can use it if i can.</p>
-            <p>so far there is nothing</p>
+            <h1>Hello Welcome</h1>
             <button onClick={() => navigate('/notes/signup')}>Get Started</button>
         </div>
     );
