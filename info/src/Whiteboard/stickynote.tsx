1819c7127c9f1e3adb7c7bc7658f8418ea775f17
Alexhamill <alexander.hamill@icloud.com>
2025-04-18 18:24:07 -0400

    added the expandor alotho is doesnt do anything yet

--- info/src/Whiteboard/stickynote.tsx
+++ info/src/Whiteboard/stickynote.tsx
@@ -15,32 +15,41 @@ interface StickyNote {
 
 const Sticky: React.FC<{ note: StickyNote, idd: string }> = ({ note, idd }) => {
     const { user, userData } = useUser();
-    var xsize = 0;
-    var ysize = 0;
+    // var xsize = 0;
+    // var ysize = 0;
     
 
     function touchstart(e: React.MouseEvent<HTMLDivElement>) {
-        xsize = parseFloat(e.currentTarget.style.height.replace("px", "")) / 2;
-        ysize = parseFloat(e.currentTarget.style.width.replace("px", "")) / 2;
-        e.currentTarget.style.zIndex = "1000";
+        // xsize = parseFloat(e.currentTarget.style.height.replace("px", "")) / 2;
+        // ysize = parseFloat(e.currentTarget.style.width.replace("px", "")) / 2;
+        const parent = e.currentTarget.parentElement;
+        if (parent) {
+            parent.style.zIndex = "1000"
+        }
         e.preventDefault();
     }
     function touchmove(e: React.MouseEvent<HTMLDivElement>) {
         e.preventDefault();
         const isMouseDown = e.buttons === 1;
         if (isMouseDown) {
-            (e.currentTarget as HTMLDivElement).style.left = `${e.clientX - 50}px`;
-            (e.currentTarget as HTMLDivElement).style.top = `${e.clientY - 50}px`;
+            const parent = (e.currentTarget as HTMLDivElement).parentElement;
+            if (parent) {
+                parent.style.top = `${e.clientY - 10}px`;
+                parent.style.left = `${e.clientX - 10}px`;
+            }
         }
         
     }
 
     function touchend(e: React.MouseEvent<HTMLDivElement>) {
-        e.currentTarget.style.zIndex = "1";
         e.preventDefault();
-        const ref = nesteddoc("Sticky-notes", e.currentTarget.id);
-        console.log(e.currentTarget.id)
-        updateDoc(ref, { left: e.currentTarget.style.left, top: e.currentTarget.style.top });
+        const parent = e.currentTarget.parentElement;
+        if (parent) {
+            parent.style.zIndex = "1"
+        const ref = nesteddoc("Sticky-notes", parent.id);
+        console.log(parent.id)
+        updateDoc(ref, { left: parent.style.left, top: parent.style.top });
+        }
     }
 
 
@@ -57,16 +66,18 @@ const Sticky: React.FC<{ note: StickyNote, idd: string }> = ({ note, idd }) => {
                 height: note.height,
                 // transition: "left 0.02s, top 0.02s",
             }}
-            onMouseDown = {(e) => { touchstart(e) }}
-            onMouseMove = {(e) => { touchmove(e)}}
-            onMouseUp = {(e) => { touchend(e) }}
-            onMouseLeave={(e => { touchend(e) })}
-
             className="sticky-note">
 
-            <p style={{fontSize:"100%"}}>
+                <div className="sticknote-mover" 
+                    onMouseDown = {(e) => { touchstart(e) }}
+                    onMouseMove = {(e) => { touchmove(e)}}
+                    onMouseUp = {(e) => { touchend(e) }}
+                    onMouseLeave={(e => { touchend(e) })}
+                ></div>
+                <div className="stickynote-expander"></div>
+                <p style={{fontSize:"100%"}}>
                 {note.content}
-            </p>
+                </p>
             
         </div>
     );
